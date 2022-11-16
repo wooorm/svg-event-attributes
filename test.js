918e5f97b9976aec32ed23c2cd48cdf14d1bddfc
Titus Wormer <tituswormer@gmail.com>
2022-11-16 13:39:25 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,20 +1,16 @@
-import assert from 'node:assert'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {svgEventAttributes} from './index.js'
 
-test('svgEventAttributes', function (t) {
-  t.ok(Array.isArray(svgEventAttributes), 'should be an array')
+test('svgEventAttributes', function () {
+  assert.ok(Array.isArray(svgEventAttributes), 'should be an array')
 
-  t.doesNotThrow(function () {
-    let index = -1
+  let index = -1
 
-    while (++index < svgEventAttributes.length) {
-      const prop = svgEventAttributes[index]
-      assert.equal(typeof prop, 'string', prop + ' should be string')
-      assert.strictEqual(prop, prop.trim(), prop + ' should be trimmed')
-      assert.ok(/^on[a-z]+$/.test(prop), prop + ' should be `a-z`')
-    }
-  }, 'name should be lowercase, trimmed, alphabetical strings')
-
-  t.end()
+  while (++index < svgEventAttributes.length) {
+    const prop = svgEventAttributes[index]
+    assert.equal(typeof prop, 'string', prop + ' should be string')
+    assert.strictEqual(prop, prop.trim(), prop + ' should be trimmed')
+    assert.ok(/^on[a-z]+$/.test(prop), prop + ' should be `a-z`')
+  }
 })
