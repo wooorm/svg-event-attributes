6a52d149213057d2a261b95a5aa50015323381d2
Titus Wormer <tituswormer@gmail.com>
2022-11-16 13:44:30 +0100

    Refactor build script

--- build.js
+++ build.js
@@ -1,121 +1,87 @@
-import assert from 'node:assert'
-import fs from 'node:fs'
-import https from 'node:https'
-import {bail} from 'bail'
-import concatStream from 'concat-stream'
+import assert from 'node:assert/strict'
+import fs from 'node:fs/promises'
+import fetch from 'node-fetch'
 import alphaSort from 'alpha-sort'
-import {unified} from 'unified'
-import rehypeParse from 'rehype-parse'
+import {fromHtml} from 'hast-util-from-html'
 import {select, selectAll} from 'hast-util-select'
 import {toString} from 'hast-util-to-string'
 import {isEventHandler} from 'hast-util-is-event-handler'
 
-const processor = unified().use(rehypeParse)
-
-let actual = 0
-const expected = 3
-
 /** @type {Set<string>} */
 const set = new Set()
 
-https.get('https://www.w3.org/TR/SVG11/attindex.html', (response) => {
-  response
-    .pipe(
-      concatStream((buf) => {
-        const tree = processor.parse(buf)
-        const nodes = selectAll('.property-table tr', tree)
-        let index = -1
+// SVG 1.1.
+const response1 = await fetch('https://www.w3.org/TR/SVG11/attindex.html')
+const text1 = await response1.text()
 
-        if (nodes.length === 0) {
-          throw new Error('Couldn’t find rows in SVG 1')
-        }
+const nodes1 = selectAll('.property-table tr', fromHtml(text1))
+let index = -1
 
-        while (++index < nodes.length) {
-          const names = selectAll('.attr-name', nodes[index])
-          let offset = -1
+if (nodes1.length === 0) {
+  throw new Error('Couldn’t find rows in SVG 1')
+}
 
-          while (++offset < names.length) {
-            const value = toString(names[offset]).replace(/[‘’]/g, '')
+while (++index < nodes1.length) {
+  const names = selectAll('.attr-name', nodes1[index])
+  let offset = -1
 
-            if (isEventHandler(value)) set.add(value)
-          }
-        }
+  while (++offset < names.length) {
+    const value = toString(names[offset]).replace(/[‘’]/g, '')
 
-        done()
-      })
-    )
-    .on('error', bail)
-})
-
-https.get('https://www.w3.org/TR/SVGTiny12/attributeTable.html', (response) => {
-  response
-    .pipe(
-      concatStream((buf) => {
-        const tree = processor.parse(buf)
-        const nodes = selectAll('#attributes .attribute', tree)
-        let index = -1
-
-        if (nodes.length === 0) {
-          throw new Error('Couldn’t find nodes in SVG Tiny')
-        }
-
-        while (++index < nodes.length) {
-          const name = select('.attribute-name', nodes[index])
-          assert(name, 'expeted `name` node')
-          const value = toString(name)
-
-          if (isEventHandler(value)) set.add(value)
-        }
-
-        done()
-      })
-    )
-    .on('error', bail)
-})
-
-https.get('https://www.w3.org/TR/SVG2/attindex.html', (response) => {
-  response
-    .pipe(
-      concatStream((buf) => {
-        const tree = processor.parse(buf)
-        const nodes = selectAll('tbody tr', tree)
-        let index = -1
-
-        if (nodes.length === 0) {
-          throw new Error('Couldn’t find nodes in SVG 2')
-        }
-
-        while (++index < nodes.length) {
-          const name = select('.attr-name span', nodes[index])
-          assert(name, 'expeted `name` node')
-          const value = toString(name)
-
-          if (isEventHandler(value)) set.add(value)
-        }
-
-        done()
-      })
-    )
-    .on('error', bail)
-})
-
-function done() {
-  actual++
-
-  if (actual === expected) {
-    fs.writeFile(
-      'index.js',
-      [
-        '/**',
-        ' * List of SVG event handler attributes.',
-        ' *',
-        ' * @type {Array<string>}',
-        ' */',
-        'export const svgEventAttributes = ' +
-          JSON.stringify([...set].sort(alphaSort()), null, 2),
-        ''
-      ].join('\n'),
-      bail
-    )
+    if (isEventHandler(value)) set.add(value)
   }
 }
+
+// SVG Tiny.
+const responseTiny = await fetch(
+  'https://www.w3.org/TR/SVGTiny12/attributeTable.html'
+)
+const textTiny = await responseTiny.text()
+
+const nodesTiny = selectAll('#attributes .attribute', fromHtml(textTiny))
+index = -1
+
+if (nodesTiny.length === 0) {
+  throw new Error('Couldn’t find nodes in SVG Tiny')
+}
+
+while (++index < nodesTiny.length) {
+  const name = select('.attribute-name', nodesTiny[index])
+  assert(name, 'expeted `name` node')
+  const value = toString(name)
+
+  if (isEventHandler(value)) set.add(value)
+}
+
+// SVG 2.
+const response2 = await fetch('https://www.w3.org/TR/SVG2/attindex.html')
+const text2 = await response2.text()
+
+const nodes2 = selectAll('tbody tr', fromHtml(text2))
+index = -1
+
+if (nodes2.length === 0) {
+  throw new Error('Couldn’t find nodes in SVG 2')
+}
+
+while (++index < nodes2.length) {
+  const name = select('.attr-name span', nodes2[index])
+  assert(name, 'expeted `name` node')
+  const value = toString(name)
+
+  if (isEventHandler(value)) set.add(value)
+}
+
+await fs.writeFile(
+  'index.js',
+  [
+    '/**',
+    ' * List of SVG event handler attributes.',
+    ' *',
+    ' * @type {Array<string>}',
+    ' */',
+    'export const svgEventAttributes = ' +
+      JSON.stringify([...set].sort(alphaSort()), null, 2),
+    ''
+  ].join('\n')
+)
